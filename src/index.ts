82c3ada467f4a2a4633a967320254c96162b2332
Leandro Gavidia Santamaria <leandro.d3v@gmail.com>
2024-04-22 01:59:37 -0400

    feat: shyft mint option

--- src/index.ts
+++ src/index.ts
@@ -1,369 +1,146 @@
-import fetch from "node-fetch"
-import {
-    AccountMeta,
-    Connection,
-    Keypair,
-    PublicKey,
-    Transaction,
-    clusterApiUrl,
-    sendAndConfirmTransaction,
-} from "@solana/web3.js"
-import {
-    airdropSolIfNeeded,
-    createNftMetadata,
-    CollectionDetails,
-    getOrCreateCollectionNFT,
-    getPriorityFees,
-} from "./utils"
-import {
-    SPL_ACCOUNT_COMPRESSION_PROGRAM_ID,
-    ValidDepthSizePair,
-    createAllocTreeIx,
-    SPL_NOOP_PROGRAM_ID,
-    ConcurrentMerkleTreeAccount,
-} from "@solana/spl-account-compression"
-import {
-    PROGRAM_ID as BUBBLEGUM_PROGRAM_ID,
-    createCreateTreeInstruction,
-    createMintToCollectionV1Instruction,
-    createTransferInstruction,
-    getLeafAssetId,
-} from "@metaplex-foundation/mpl-bubblegum"
-import { PROGRAM_ID as TOKEN_METADATA_PROGRAM_ID } from "@metaplex-foundation/mpl-token-metadata"
-// import { BN } from "@project-serum/anchor"
-import { base58 } from '@metaplex-foundation/umi/serializers';
+import { ShyftSdk, Network } from '@shyft-to/js';
 import dotenv from "dotenv"
 dotenv.config()
 
 async function main() {
-    if (!process.env?.PAYER_SECRET_KEY) {
-        throw new Error("Missing PAYER_SECRET_KEY .env variable")
+    if (!process.env.SHYFT_API_KEY) {
+        throw new Error("Missing SHYFT_API_KEY environment variable")
+    } else if (!process.env.RECEIVER_WALLET_ADDRESS) {
+        throw new Error("Missing RECEIVER_WALLET_ADDRESS environment variable")
+    } else if (!process.env.PAYER_WALLET_ADDRESS) {
+        throw new Error("Missing PAYER_WALLET_ADDRESS environment variable")
     }
+    const receiverWalletAddress = process.env.RECEIVER_WALLET_ADDRESS
+    const payerWalletAddress = process.env.PAYER_WALLET_ADDRESS
+    const network = Network.Devnet;
+    const collectionImageUrl = "https://bafkreidattjvuxzbi7dasz5iyzs5i2wj6b3ckvcr5w7li7bj5s7mvol674.ipfs.nftstorage.link/";
+    const solscanOption = network === Network.Devnet ? "?cluster=devnet" : null
+    const externalWebsiteUrl = "https://twitter.com/El_Noviciado";
 
-    const rpc = process.env.RPC_URL ? process.env.RPC_URL : clusterApiUrl("devnet")
+    const cnftImage = "https://nftstorage.link/ipfs/bafkreiccybwv72sz5n7muomxjyaaidv55c567s275ahw3msq4jjvlzfc64"
 
-    const connection = new Connection(rpc, "confirmed")
-
-    const secretKey = base58.serialize(process.env?.PAYER_SECRET_KEY)
-    const wallet = Keypair.fromSecretKey(new Uint8Array([222,36,60,223,62,71,202,67,207,11,104,88,201,14,158,120,61,156,58,14,14,207,104,20,143,203,8,213,101,190,200,176,243,94,56,254,202,112,175,179,197,241,78,12,89,124,1,125,147,48,236,248,195,112,139,166,55,209,165,199,232,202,37,1]))
-
-    console.log("Payer address:", wallet.publicKey.toBase58());
-    // airdropSolIfNeeded(wallet.publicKey)
-
-    const maxDepthSizePair: ValidDepthSizePair = {
-        maxDepth: 3,
-        maxBufferSize: 8,
-    }
-
-    const canopyDepth = 0
-
-    const treeAddress = await createAndInitializeTree(
-        connection,
-        wallet,
-        maxDepthSizePair,
-        canopyDepth
-    )
-
-    const collectionNft = await getOrCreateCollectionNFT(connection, wallet)
-
-    await mintCompressedNftToCollection(
-        connection,
-        wallet,
-        treeAddress,
-        collectionNft,
-        2 ** maxDepthSizePair.maxDepth
-    )
-
-    // Log NFT details to illustrate Read API
-    // await logNftDetails(treeAddress, 2 ** maxDepthSizePair.maxDepth)
-
-    // const receiver = new PublicKey(wallet.publicKey.toBase58())
-
-    // // Transfer first cNFT to random receiver to illustrate transfers
-    // await transferNft(
-    //     connection,
-    //     await getLeafAssetId(treeAddress, new BN(0)),
-    //     wallet,
-    //     receiver
-    // )
-}
-
-async function createAndInitializeTree(
-    connection: Connection,
-    payer: Keypair,
-    maxDepthSizePair: ValidDepthSizePair,
-    canopyDepth: number
-) {
-    const treeKeypair = Keypair.generate()
-    const [treeAuthority, _bump] = PublicKey.findProgramAddressSync(
-        [treeKeypair.publicKey.toBuffer()],
-        BUBBLEGUM_PROGRAM_ID
-    )
-    const allocTreeIx = await createAllocTreeIx(
-        connection,
-        treeKeypair.publicKey,
-        payer.publicKey,
-        maxDepthSizePair,
-        canopyDepth
-    )
-    const createTreeIx = createCreateTreeInstruction(
-        {
-            treeAuthority,
-            merkleTree: treeKeypair.publicKey,
-            payer: payer.publicKey,
-            treeCreator: payer.publicKey,
-            logWrapper: SPL_NOOP_PROGRAM_ID,
-            compressionProgram: SPL_ACCOUNT_COMPRESSION_PROGRAM_ID,
-        },
-        {
-            maxBufferSize: maxDepthSizePair.maxBufferSize,
-            maxDepth: maxDepthSizePair.maxDepth,
-            public: false,
-        }
-    )
-
-    const priorityFeesInstruction = await getPriorityFees([allocTreeIx.programId.toBase58(), createTreeIx.programId.toBase58()])
-
-    if (priorityFeesInstruction === undefined) {
-      throw new Error("Error getting transaction priority fee transaction")
-    }
-
-    const tx = new Transaction().add(priorityFeesInstruction).add(allocTreeIx, createTreeIx)
-    tx.feePayer = payer.publicKey
+    const mintNumbers = 2;
 
     try {
-        const txSignature = await sendAndConfirmTransaction(
-            connection,
-            tx,
-            [treeKeypair, payer],
-            {
-                commitment: "finalized",
-                skipPreflight: true,
-            }
-        )
+        const shyft = new ShyftSdk({ apiKey: process.env.SHYFT_API_KEY, network: network });
+        const balance = await shyft.wallet.getBalance({ wallet: payerWalletAddress });
+        console.log(`Payer current balance: ${balance} SOL`)
 
-        console.log(`https://explorer.solana.com/tx/${txSignature}?cluster=devnet`)
-
-        console.log("Tree Address:", treeKeypair.publicKey.toBase58())
-
-        return treeKeypair.publicKey
-    } catch (err: any) {
-        console.error("\nFailed to create merkle tree:", err)
-        throw err
-    }
-}
-
-async function mintCompressedNftToCollection(
-    connection: Connection,
-    payer: Keypair,
-    treeAddress: PublicKey,
-    collectionDetails: CollectionDetails,
-    amount: number
-) {
-    // Derive the tree authority PDA ('TreeConfig' account for the tree account)
-    const [treeAuthority] = PublicKey.findProgramAddressSync(
-        [treeAddress.toBuffer()],
-        BUBBLEGUM_PROGRAM_ID
-    )
-
-    // Derive the bubblegum signer, used by the Bubblegum program to handle "collection verification"
-    // Only used for `createMintToCollectionV1` instruction
-    const [bubblegumSigner] = PublicKey.findProgramAddressSync(
-        [Buffer.from("collection_cpi", "utf8")],
-        BUBBLEGUM_PROGRAM_ID
-    )
-
-    for (let i = 0; i < amount; i++) {
-        // Compressed NFT Metadata
-        const compressedNFTMetadata = createNftMetadata(payer.publicKey, i, collectionDetails.mint)
-
-        // Create the instruction to "mint" the compressed NFT to the tree
-        const mintIx = createMintToCollectionV1Instruction(
-            {
-                payer: payer.publicKey, // The account that will pay for the transaction
-                merkleTree: treeAddress, // The address of the tree account
-                treeAuthority, // The authority of the tree account, should be a PDA derived from the tree account address
-                treeDelegate: payer.publicKey, // The delegate of the tree account, should be the same as the tree creator by default
-                leafOwner: payer.publicKey, // The owner of the compressed NFT being minted to the tree
-                leafDelegate: payer.publicKey, // The delegate of the compressed NFT being minted to the tree
-                collectionAuthority: payer.publicKey, // The authority of the "collection" NFT
-                collectionAuthorityRecordPda: BUBBLEGUM_PROGRAM_ID, // Must be the Bubblegum program id
-                collectionMint: collectionDetails.mint, // The mint of the "collection" NFT
-                collectionMetadata: collectionDetails.metadata, // The metadata of the "collection" NFT
-                editionAccount: collectionDetails.masterEditionAccount, // The master edition of the "collection" NFT
-                compressionProgram: SPL_ACCOUNT_COMPRESSION_PROGRAM_ID,
-                logWrapper: SPL_NOOP_PROGRAM_ID,
-                bubblegumSigner,
-                tokenMetadataProgram: TOKEN_METADATA_PROGRAM_ID,
-            },
-            {
-                metadataArgs: Object.assign(compressedNFTMetadata, {
-                    collection: { key: collectionDetails.mint, verified: false },
-                }),
-            }
-        )
-
-        const priorityFeesInstruction = await getPriorityFees([mintIx.programId.toBase58()])
-
-        if (priorityFeesInstruction === undefined) {
-          throw new Error("Error getting transaction priority fee transaction")
-        }
-
-        try {
-            // Create new transaction and add the instruction
-            const tx = new Transaction().add(priorityFeesInstruction).add(mintIx)
-
-            // Set the fee payer for the transaction
-            tx.feePayer = payer.publicKey
-
-            // Send the transaction
-            const txSignature = await sendAndConfirmTransaction(
-                connection,
-                tx,
-                [payer],
-                { commitment: "finalized", skipPreflight: true }
-            )
-
-            console.log(
-                `https://explorer.solana.com/tx/${txSignature}?cluster=devnet`
-            )
-        } catch (err) {
-            console.error("\nFailed to mint compressed NFT:", err)
-            throw err
-        }
-    }
-}
-
-// async function logNftDetails(treeAddress: PublicKey, nftsMinted: number) {
-//     if (!process.env.RPC_URL) {
-//         throw new Error("Missing RPC_URL Enviroment variable")
-//     }
-//     for (let i = 0; i < nftsMinted; i++) {
-//         const assetId = await getLeafAssetId(treeAddress, new BN(i))
-//         console.log("Asset ID:", assetId.toBase58())
-//         const response = await fetch(process.env.RPC_URL, {
-//             method: "POST",
-//             headers: { "Content-Type": "application/json" },
-//             body: JSON.stringify({
-//                 jsonrpc: "2.0",
-//                 id: "my-id",
-//                 method: "getAsset",
-//                 params: {
-//                     id: assetId,
-//                 },
-//             }),
-//         })
-//         const { result } = await response.json() as any
-//         console.log(JSON.stringify(result, null, 2))
-//     }
-// }
-
-async function transferNft(
-    connection: Connection,
-    assetId: PublicKey,
-    sender: Keypair,
-    receiver: PublicKey
-) {
-    if (!process.env.RPC_URL) {
-        throw new Error("Missing RPC_URL Enviroment variable")
-    }
-    try {
-        const assetDataResponse = await fetch(process.env.RPC_URL, {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({
-                jsonrpc: "2.0",
-                id: "my-id",
-                method: "getAsset",
-                params: {
-                    id: assetId,
-                },
-            }),
+        console.log("Creating Collection URI...")
+        const { uri: collectionUri } = await shyft.storage.createMetadata({
+            creator: payerWalletAddress,
+            description: "This pass is the first one to be delivered to 420 members of the club.",
+            name: "Noviciado OG Pass",
+            symbol: "NOP",
+            external_url: externalWebsiteUrl,
+            image: collectionImageUrl,
+            attributes: [{trait_type: "collection", value: "Noviciado OG Pass"}],
         })
-        const assetData = (await assetDataResponse.json() as any).result
 
-        const assetProofResponse = await fetch(process.env.RPC_URL, {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({
-                jsonrpc: "2.0",
-                id: "my-id",
-                method: "getAssetProof",
-                params: {
-                    id: assetId,
-                },
-            }),
+        console.log("URI URL:", collectionUri)
+
+        console.log("Creating collection...")
+        const collectionRes = await shyft.nft.createFromMetadata({
+            metadataUri: collectionUri,
+            receiver: receiverWalletAddress,
+            feePayer: payerWalletAddress,
+            network: network,
         })
-        const assetProof = (await assetProofResponse.json() as any).result
 
-        const treePublicKey = new PublicKey(assetData.compression.tree)
+        const collectionEncodedTransaction = collectionRes.encoded_transaction;
+        const collectionAddress = collectionRes.mint;
 
-        const treeAccount = await ConcurrentMerkleTreeAccount.fromAccountAddress(
-            connection,
-            treePublicKey
-        )
+        console.log("Signing collection transaction...")
+        const collectionTransactionRes = await shyft.txnRelayer.signMany({
+            network: network,
+            encodedTransactions: [collectionEncodedTransaction],
+            commitment: "confirmed"
+        })
 
-        const canopyDepth = treeAccount.getCanopyDepth() || 0
+        const collectionSignature = collectionTransactionRes[0].signature;
 
-        const proofPath: AccountMeta[] = assetProof.proof
-            .map((node: string) => ({
-                pubkey: new PublicKey(node),
-                isSigner: false,
-                isWritable: false,
-            }))
-            .slice(0, assetProof.proof.length - canopyDepth)
+        console.log(`Collection's signature: https://solscan.io/tx/${collectionSignature}${solscanOption}`);
+        console.log(`Collection mint address: ${collectionAddress}`)
 
-        const treeAuthority = treeAccount.getAuthority()
-        const leafOwner = new PublicKey(assetData.ownership.owner)
-        const leafDelegate = assetData.ownership.delegate
-            ? new PublicKey(assetData.ownership.delegate)
-            : leafOwner
 
-        const transferIx = createTransferInstruction(
-            {
-                merkleTree: treePublicKey,
-                treeAuthority,
-                leafOwner,
-                leafDelegate,
-                newLeafOwner: receiver,
-                logWrapper: SPL_NOOP_PROGRAM_ID,
-                compressionProgram: SPL_ACCOUNT_COMPRESSION_PROGRAM_ID,
-                anchorRemainingAccounts: proofPath,
+        console.log("Creating merkle tree...")
+        const merkleTree = await shyft.nft.compressed.createMerkleTree({
+            network: network,
+            walletAddress: payerWalletAddress,
+            maxDepthSizePair: {
+                maxDepth: 3,
+                maxBufferSize: 8
             },
-            {
-                root: [...new PublicKey(assetProof.root.trim()).toBytes()],
-                dataHash: [
-                    ...new PublicKey(assetData.compression.data_hash.trim()).toBytes(),
-                ],
-                creatorHash: [
-                    ...new PublicKey(assetData.compression.creator_hash.trim()).toBytes(),
-                ],
-                nonce: assetData.compression.leaf_id,
-                index: assetData.compression.leaf_id,
-            }
-        )
+            canopyDepth: 0,
+            feePayer: payerWalletAddress
+        })
 
-        const priorityFeesInstruction = await getPriorityFees([transferIx.programId.toBase58()])
+        const merkleTreeEncodedTransaction = merkleTree.encoded_transaction;
+        const merkleTreeAddress = merkleTree.tree
 
-        if (priorityFeesInstruction === undefined) {
-          throw new Error("Error getting transaction priority fee transaction")
+        console.log("Signing merkle tree transaction...")
+        const merkleTreeTransactionRes = await shyft.txnRelayer.signMany({
+            network: network,
+            encodedTransactions: [merkleTreeEncodedTransaction],
+            commitment: "confirmed"
+        })
+
+        const merkleTreeSignature = merkleTreeTransactionRes[0].signature;
+
+        console.log(`Merkle Tree's signature: https://solscan.io/tx/${merkleTreeSignature}${solscanOption}`);
+        console.log(`Merkle tree address: ${merkleTreeAddress}`)
+
+
+        console.log("creating cNFT metadata...")
+        const { uri: cnftUri } = await shyft.storage.createMetadata({
+            creator: payerWalletAddress,
+            description: "This pass is the first one to be delivered to 420 members of the club.",
+            name: "Noviciado OG Pass",
+            symbol: "NOP",
+            external_url: externalWebsiteUrl,
+            image: cnftImage,
+            attributes: [{trait_type: "collection", value: "Noviciado OG Pass"}],
+        })
+
+
+        console.log(`cNFT's metadata URL: ${cnftUri}`);
+
+
+        console.log("Minting cNFTs...")
+        
+        const mintEncodedTransactions = [];
+
+        for (let i = 0; i < mintNumbers; i++) {
+            const cnftMint = await shyft.nft.compressed.mint({
+                network: network,
+                creatorWallet: payerWalletAddress,
+                merkleTree: merkleTreeAddress,
+                metadataUri: cnftUri,
+                collectionAddress: collectionAddress,
+                isMutable: true,
+                feePayer: payerWalletAddress,
+                receiver: receiverWalletAddress,
+            })
+
+            const cnftMintEncodedTransaction = cnftMint.encoded_transaction;
+            const cnftMintAddress = cnftMint.mint;
+
+            mintEncodedTransactions.push(cnftMintEncodedTransaction)
+            console.log(`cNFT Mint address ${i + 1}: ${cnftMintAddress}`)
         }
+        
+        console.log("Signing cNFTs mint transaction...")
+        const cnftsMintTransactionRes = await shyft.txnRelayer.signMany({
+            network: network,
+            encodedTransactions: mintEncodedTransactions,
+            commitment: "confirmed"
+        })
 
-        const tx = new Transaction().add(priorityFeesInstruction).add(transferIx)
-        tx.feePayer = sender.publicKey
-        const txSignature = await sendAndConfirmTransaction(
-            connection,
-            tx,
-            [sender],
-            {
-                commitment: "confirmed",
-                skipPreflight: true,
-            }
-        )
-        console.log(`https://explorer.solana.com/tx/${txSignature}?cluster=devnet`)
-    } catch (err: any) {
-        console.error("\nFailed to transfer nft:", err)
-        throw err
+        const cnftsMintSignature = cnftsMintTransactionRes[0].signature;
+
+        console.log(`cNFT's signature: https://solscan.io/tx/${cnftsMintSignature}${solscanOption}`);
+    } catch (e) {
+        console.error(e)
     }
 }
 
